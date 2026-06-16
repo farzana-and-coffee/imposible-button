84b5cf0dd874d07a4f736769da54e1e0463d0806
Farzana <your@email.com>
2026-06-16 18:26:20 +0300

    added js script

--- script.js
+++ script.js
@@ -1,2 +1,54 @@
+//Getting references from index.html
 const introScreen = document.getElementById('intro');
-const introScreen = document.getElementById('game');
+const gameScreen = document.getElementById('game'); 
+
+//after the intro, dispaly game screen
+setTimeout(() => {
+    introScreen.style.display = 'none';
+    gameScreen.style.display = 'block';
+}, 4300);
+
+//get button
+const button = document.getElementById('imp-button');
+
+//game state
+const gameState = [
+    {bg: '#D15F43', btnColor: '#CDDB03' },   
+    { bg: '#367091', btnColor: '#E4AFC2' },  
+    { bg: '#43D15D', btnColor: '#E7CC00' },   
+    { bg: '#FF7A0D', btnColor: '#FF7A0D' },   
+    { bg: '#FFFFFF', btnColor: '#FF0000' },  
+    { bg: '#004DFF', btnColor: '#03E1C7' },  
+    { bg: '#FFE500', btnColor: '#FA7407' },  
+    { bg: '#A14468', btnColor: '#FF8623' },   
+    { bg: '#4DB9C5', btnColor: '#EE383B' },   
+    { bg: '#701112', btnColor: '#FFFFFF' },   
+    { bg: '#2D7C1D', btnColor: '#E3783F' },  
+    { bg: '#FF88D5', btnColor: '#A0F190' },   
+    { bg: '#72350F', btnColor: '#7687FB' },   
+    { bg: '#F58D43', btnColor: '#DB41E4' }, 
+    { bg: '#0F2459', btnColor: '#EBA972' },   
+    { bg: '#199C54', btnColor: '#5D8BFF' },  
+    { bg: '#AC29CD', btnColor: '#F7C26C' },  
+    { bg: '#FFFFFF', btnColor: '#FFE500' },  
+    { bg: '#7687FB', btnColor: '#FFE500' },  
+    { bg: '#D980FF', btnColor: '#ED0202' },   
+    { bg: '#B0F296', btnColor: '#F4BCF7' },   
+    { bg: '#E75E08', btnColor: '#FBFF00' },  
+    { bg: '#8B4141', btnColor: '#FFACDD' },
+];
+
+//track frame
+let currentFrame = 0;
+
+//function that moves the button to random spot
+function moveButton(){
+    const randomTop = Math.random() *80+10;
+    const randomLeft =Math.random() *80+10;
+    button.style.top = randomTop + '%';
+    button.style.left = randomLeft+ '%';
+
+}
+
+// button run when the mouse hover the button
+button.addEventListener('mouseover', moveButton);
